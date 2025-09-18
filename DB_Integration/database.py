f8988c77c0456391981ba516e39f756694b24b99
palaashgupta <gpalaash@gmail.com>
2025-09-18 22:09:05 +0200

    Integrating POSTGRESQL, SQL and Initial phase of alembic(Update tables instead of creating new again and again)

--- DB_Integration/database.py
+++ DB_Integration/database.py
@@ -2,9 +2,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQL_ALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+### SQLITE3 Database
+#SQL_ALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'  SQLITE3 Database
+# engine = create_engine(SQL_ALCHEMY_DATABASE_URL, connect_args = {"check_same_thread": False}) sqlite3 Database
 
-engine = create_engine(SQL_ALCHEMY_DATABASE_URL, connect_args = {"check_same_thread": False})
+
+### PostgreSQL Database
+#SQL_ALCHEMY_DATABASE_URL = 'postgresql://postgres:Tutafuta%40123@localhost/TodopplicationDatabase' #PostgreSQL Database
+#engine = create_engine(SQL_ALCHEMY_DATABASE_URL) #PostgreSQL Database
+
+### MySQL Database
+SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:Tutafuta%40123@127.0.0.1:3306/todoapplicationdatabase' #MySQL Database
+engine = create_engine(SQLALCHEMY_DATABASE_URL) #MySQL Database
 
 SessionLocal = sessionmaker(autocommit = False, autoflush=False , bind = engine)
 
