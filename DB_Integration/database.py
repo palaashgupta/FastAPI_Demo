4a2bd0d159064b385d055a1bb7e7e21ecc6ccd94
palaashgupta <gpalaash@gmail.com>
2025-09-19 17:40:59 +0200

    Updating tables with alembic

--- DB_Integration/database.py
+++ DB_Integration/database.py
@@ -3,8 +3,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 ### SQLITE3 Database
-#SQL_ALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'  SQLITE3 Database
-# engine = create_engine(SQL_ALCHEMY_DATABASE_URL, connect_args = {"check_same_thread": False}) sqlite3 Database
+SQL_ALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'  #SQLITE3 Database
+engine = create_engine(SQL_ALCHEMY_DATABASE_URL, connect_args = {"check_same_thread": False}) # sqlite3 Database
 
 
 ### PostgreSQL Database
@@ -12,8 +12,8 @@ from sqlalchemy.ext.declarative import declarative_base
 #engine = create_engine(SQL_ALCHEMY_DATABASE_URL) #PostgreSQL Database
 
 ### MySQL Database
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:Tutafuta%40123@127.0.0.1:3306/todoapplicationdatabase' #MySQL Database
-engine = create_engine(SQLALCHEMY_DATABASE_URL) #MySQL Database
+#SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:Tutafuta%40123@127.0.0.1:3306/todoapplicationdatabase' #MySQL Database
+#engine = create_engine(SQLALCHEMY_DATABASE_URL) #MySQL Database
 
 SessionLocal = sessionmaker(autocommit = False, autoflush=False , bind = engine)
 
