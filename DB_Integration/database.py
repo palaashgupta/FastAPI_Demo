3b7371a9cf0a13c3dfccc35197d2ff5b0eea1419
Palaash <44490183+palaashgupta@users.noreply.github.com>
2025-09-19 16:18:18 +0200

    Update database.py

--- DB_Integration/database.py
+++ DB_Integration/database.py
@@ -8,11 +8,11 @@ from sqlalchemy.ext.declarative import declarative_base
 
 
 ### PostgreSQL Database
-#SQL_ALCHEMY_DATABASE_URL = 'postgresql://postgres:Tutafuta%40123@localhost/TodopplicationDatabase' #PostgreSQL Database
+#SQL_ALCHEMY_DATABASE_URL = 'postgresql://postgres:#####%40123@localhost/TodopplicationDatabase' #PostgreSQL Database
 #engine = create_engine(SQL_ALCHEMY_DATABASE_URL) #PostgreSQL Database
 
 ### MySQL Database
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:Tutafuta%40123@127.0.0.1:3306/todoapplicationdatabase' #MySQL Database
+SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:####%40123@127.0.0.1:3306/todoapplicationdatabase' #MySQL Database
 engine = create_engine(SQLALCHEMY_DATABASE_URL) #MySQL Database
 
 SessionLocal = sessionmaker(autocommit = False, autoflush=False , bind = engine)
